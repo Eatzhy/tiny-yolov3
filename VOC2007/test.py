bf30366b15d0164f49a22b045bef038b028b8012
Young Zhan <32692556+Eatzhy@users.noreply.github.com>
2018-11-26 18:34:49 +0800

    Add files via upload

---
+++ VOC2007/test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 27 12:59:20 2018
+
+@author: Administrator
+"""
+
+import os
+import random
+
+trainval_percent = 0.1
+train_percent = 0.9
+xmlfilepath = 'Annotations'
+txtsavepath = 'ImageSets\Main'
+total_xml = os.listdir(xmlfilepath)
+
+num = len(total_xml)
+list = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+trainval = random.sample(list, tv)
+train = random.sample(trainval, tr)
+
+ftrainval = open('ImageSets/Main/trainval.txt', 'w')
+ftest = open('ImageSets/Main/test.txt', 'w')
+ftrain = open('ImageSets/Main/train.txt', 'w')
+fval = open('ImageSets/Main/val.txt', 'w')
+
+for i in list:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        ftrainval.write(name)
+        if i in train:
+            ftest.write(name)
+        else:
+            fval.write(name)
+    else:
+        ftrain.write(name)
+
+ftrainval.close()
+ftrain.close()
+fval.close()
+ftest.close()
