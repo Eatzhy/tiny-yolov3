c914100d018f46841de4d51dbecb79db36a20dad
Young Zhan <32692556+Eatzhy@users.noreply.github.com>
2018-12-14 21:19:53 +0800

    Update tiny_train.py

--- tiny_train.py
+++ tiny_train.py
@@ -8,10 +8,9 @@ import numpy as np
 import keras.backend as K
 from keras.layers import Input, Lambda
 from keras.models import Model
-from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
-from yolo3.tinymodel import preprocess_true_boxes, yolo_body, tiny_yolo_body, yolo_loss
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from yolo3.tinymodel import preprocess_true_boxes, tiny_yolo_body, yolo_loss
 from yolo3.utils import get_random_data
-
 #import cv2
 
 #需要执行的内容
@@ -19,7 +18,7 @@ def _main():
     annotation_path = 'train.txt'
     log_dir = 'logs/'
     classes_path = 'model_data/voc_classes.txt'
-    anchors_path = 'model_data/tiny_yolo_anchors.txt'
+    anchors_path = 'model_data/tiny_yolo_anchors.txt'    
     class_names = get_classes(classes_path)
     anchors = get_anchors(anchors_path)
     input_shape = (416,416)  # multiple of 32, hw
@@ -31,14 +30,23 @@ def _main():
 def train(model, annotation_path, input_shape, anchors, num_classes, log_dir='logs/'):
     model.compile(optimizer='adam', loss={
         'yolo_loss': lambda y_true, y_pred: y_pred})
-    #logging = TensorBoard(log_dir=log_dir)
-    #checkpoint = ModelCheckpoint(log_dir + "ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5",
-    #    monitor='val_loss', save_weights_only=True, save_best_only=True, period=1)
-    batch_size = 4
-    val_split = 0.1
+    #记录所有训练过程，每隔一定步数记录最大值
+    tensorboard = TensorBoard(log_dir=log_dir)
+    checkpoint = ModelCheckpoint(log_dir + "best_weights.h5",
+                                 monitor="val_loss",
+                                 mode='min',
+                                 save_weights_only=True,
+                                 save_best_only=True, 
+                                 verbose=1,
+                                 period=1)
+
+    callback_lists=[tensorboard,checkpoint]
+    batch_size = 16
+    val_split = 0.05
     with open(annotation_path) as f:
         lines = f.readlines()
     np.random.shuffle(lines)
+    
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
@@ -47,9 +55,9 @@ def train(model, annotation_path, input_shape, anchors, num_classes, log_dir='lo
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrap(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=1000, #迭代的步数
-            initial_epoch=0)
-    model.save_weights(log_dir + 'trained_weights.h5')
+            epochs=3000, #迭代的步数
+            initial_epoch=0, callbacks=callback_lists, verbose=1)
+    model.save_weights(log_dir + 'tiny-trained_weights.h5')
 
 def get_classes(classes_path):
     with open(classes_path) as f:
@@ -73,10 +81,7 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=False, freez
     y_true = [Input(shape=(h//{0:32, 1:16}[l], w//{0:32, 1:16}[l], \
         num_anchors//3, num_classes+5)) for l in range(2)]
     
-    #y_true = [Input(shape=(h//{0:32, 1:16, 2:8}[l], w//{0:32, 1:16, 2:8}[l], \
-    #    num_anchors//3, num_classes+5)) for l in range(3)]
-
-    #model_body = yolo_body(image_input, num_anchors//3, num_classes)
+  
     model_body = tiny_yolo_body(image_input, num_anchors//3, num_classes)
     print('Create YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
 
